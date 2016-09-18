2daae29e241f7e93166f67a9cd811a257540173a
Bjoern Fischer <fischerbjoern@web.de>
2016-09-18 11:53:13 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,45 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// Read the config
+var config = require('./config');
+
+// Connect to mongodb according to config.js
+var mongoose = require('mongoose');
+
+mongoose.connect(config.getMongoConnection());
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+  console.log("Connected correctly to database server");
+});
+
+
+
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
---
+++ config.js
@@ -0,0 +1,28 @@
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+
+var local_mongo_conn_str = 'mongodb://localhost:27017/conFusion';
+
+var cfenv = require('cfenv');
+
+exports.getMongoConnection = function (req, res, next) {
+
+    var mongo = process.env.VCAP_SERVICES;
+    var port = process.env.PORT || 3030;
+    
+    if (mongo) {
+        var env = JSON.parse(mongo);
+        if (env['mongodb']) {
+            mongo = env['mongodb'][0]['credentials'];
+            if (mongo.url) {
+                return mongo.url;
+            } else {
+                console.log("No mongo found");
+            }
+        }
+    }
+
+    console.log("connect to default local mongodb: " + local_mongo_conn_str);
+    return local_mongo_conn_str;
+};
